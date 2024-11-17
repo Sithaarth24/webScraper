99f27bc04c61a62b1c8a6bd48bb3945683d51254
Sithaarth24 <sithumahesh24@gmail.com>
2024-11-17 19:56:49 +0530

    changed frontend

--- index_temp.py
+++ index_temp.py
@@ -112,7 +112,7 @@ def compare_prices():
 
 @app.get('/')
 def home():
-    return render_template('index.html')
+    return render_template('index1.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
